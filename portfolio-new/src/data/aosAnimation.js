74c2c8e5d6ae3b4536b5bc0203b6cae6ece37365
jasna18 <jasnamnaz@gmail.com>
2025-12-10 23:36:23 +0400

    modified animation in about us

--- portfolio-new/src/data/aosAnimation.js
+++ portfolio-new/src/data/aosAnimation.js
@@ -1,24 +1,24 @@
-// import { onMounted } from "vue";
+export { onMounted } from "vue";
 
-// onMounted(() => {
-//   const elements = document.querySelectorAll("[data-aos]");
+onMounted(() => {
+  const elements = document.querySelectorAll("[data-aos]");
 
-//   const observer = new IntersectionObserver(
-//     (entries) => {
-//       entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//           entry.target.classList.add("aos-visible");
-//         } else {
-//           entry.target.classList.remove("aos-visible");
-//         }
-//       });
-//     },
-//     { threshold: 0.2 }
-//   );
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("aos-visible");
+        } else {
+          entry.target.classList.remove("aos-visible");
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
 
-//   elements.forEach((el) => {
-//     el.classList.add("aos-hidden");
-//     el.classList.add(el.getAttribute("data-aos"));
-//     observer.observe(el);
-//   });
-// });
+  elements.forEach((el) => {
+    el.classList.add("aos-hidden");
+    el.classList.add(el.getAttribute("data-aos"));
+    observer.observe(el);
+  });
+});
