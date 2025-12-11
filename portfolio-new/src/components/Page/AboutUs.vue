36a62d7daca998e3765fbe872a656b0daf98e550
jasna18 <jasnamnaz@gmail.com>
2025-12-11 13:16:39 +0400

    removed container from aboutus

--- portfolio-new/src/components/Page/AboutUs.vue
+++ portfolio-new/src/components/Page/AboutUs.vue
@@ -2,7 +2,7 @@
 <section id="about" class="min-h-screen"
 >
 <!-- all manin wrapper  -->
-<div class="container  px-4 lg:px-8 w-full lg:mx-autopy-4 md:py-6 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+<div class=" px-4 lg:px-8 w-full lg:mx-autopy-4 md:py-6 dark:bg-gray-900 text-gray-900 dark:text-gray-100 xl:justify-center">
       <div class="text-center mb-4">
         <h2 class="text-xl md:text-3xl font-extrabold text-[#F68537] mb-2" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
           About Me
