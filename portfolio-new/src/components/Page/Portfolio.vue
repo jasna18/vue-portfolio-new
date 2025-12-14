f4d2e391f0b561add06ee81aec89fe6acc8b532d
jasna18 <jasnamnaz@gmail.com>
2025-12-14 21:08:38 +0400

    added tabs in portfolio.vue

---
+++ portfolio-new/src/components/Page/Portfolio.vue
@@ -0,0 +1,24 @@
+<template>
+<section id="portfolio" class="min-h-screen"
+>
+<!-- all main wrapper  -->
+<div class=" px-4 lg:px-8 w-full lg:mx-autopy-4 md:py-6 dark:bg-gray-900 text-gray-900 dark:text-gray-100 xl:justify-center">
+      <div class="text-center mb-4">
+        <h2 class="text-xl md:text-3xl font-extrabold text-[#F68537] mb-2" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+           Portfolio       
+         </h2>
+        <p class="text-sm lg:text-md md:text-md text-gray-600 dark:text-gray-300" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+          <span class="font-medium">✨</span> Explore my journey through projects, certifications, and technical expertise.<span class="font-medium"> ✨</span>
+        </p>
+      </div>
+      </div>
+
+
+  <Tabs />
+      
+</section>
+</template>
+
+<script setup>
+  import Tabs from '../composobles/Tabs.vue';
+</script>
