7997a8075642a8b716ba3f88dea881b391ca2ef4
jasna18 <jasnamnaz@gmail.com>
2025-12-14 21:06:08 +0400

    added techstack contents

---
+++ portfolio-new/src/components/Page/TechStack.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-wrap gap-4 justify-center">
+    <span class="px-4 py-2 bg-indigo-100 text-indigo-600 rounded-full">
+      Vue.js
+    </span>
+    <span class="px-4 py-2 bg-indigo-100 text-indigo-600 rounded-full">
+      Tailwind CSS
+    </span>
+    <span class="px-4 py-2 bg-indigo-100 text-indigo-600 rounded-full">
+      Vite
+    </span>
+  </div>
+</template>
