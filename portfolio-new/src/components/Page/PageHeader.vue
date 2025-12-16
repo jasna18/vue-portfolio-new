07d92ba9a19f79625cf0873d2a03f2146cc04d7c
jasna18 <jasnamnaz@gmail.com>
2025-12-16 23:03:48 +0400

    modify header links

--- portfolio-new/src/components/Page/PageHeader.vue
+++ portfolio-new/src/components/Page/PageHeader.vue
@@ -1,27 +1,20 @@
 <template>
   <div :class="darkMode ? 'bg-gray-900 text-white' : 'text-black'" class="transition-colors duration-500">
-    <header 
-      :class="headerClass" 
-      :style="{ backgroundColor: darkMode ? '#111827' : '#A7C1A8' }" 
-      class="fixed top-0 w-full z-10 transition-colors duration-500"
-    >
+    <header :class="headerClass" :style="{ backgroundColor: darkMode ? '#111827' : '#A7C1A8' }"
+      class="fixed top-0 w-full z-10 transition-colors duration-500">
       <div class="w-full flex justify-between  pl-4 
                   md:w-5/6 md:max-w-7xl h-16 md:mx-auto">
-  
-        <nav class="flex justify-between items-center py-3 lg:py-3 w-full" 
-             :class="{ 'scrolled-nav': isScrolled }">
+
+        <nav class="flex justify-between items-center py-3 lg:py-3 w-full" :class="{ 'scrolled-nav': isScrolled }">
 
           <!-- Mobile menu -->
           <button class="text-2xl md:hidden" @click="toggleMenu">☰</button>
 
           <!-- Nav links -->
-          <ul
-            class="font-bold"
-            :class="[
-              menuOpen ? 'flex' : 'hidden',
-              'md:flex flex-col md:flex-row gap-4 md:gap-8 absolute md:static top-14 p-4 md:p-0 shadow md:shadow-none ml-auto mr-0 md:ml-auto md:mr-8'
-            ]"
-          >
+          <ul class="font-bold" :class="[
+            menuOpen ? 'flex' : 'hidden',
+            'md:flex flex-col md:flex-row gap-4 md:gap-8 absolute md:static top-14 p-4 md:p-0 shadow md:shadow-none ml-auto mr-0 md:ml-auto md:mr-8'
+          ]">
             <li><a href="home" @click="setActive('home')" :class="linkClass('home')">Home</a></li>
             <li><a href="#about" @click="setActive('about')" :class="linkClass('about')">About</a></li>
             <li><a href="#projects" @click="setActive('projects')" :class="linkClass('projects')">Projects</a></li>
