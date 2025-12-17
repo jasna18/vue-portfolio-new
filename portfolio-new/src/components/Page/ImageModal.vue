66d883f1080d4fdf0d6d31456d77141a6c05ba68
jasna18 <jasnamnaz@gmail.com>
2025-12-17 21:29:23 +0400

    added close button inside image

--- portfolio-new/src/components/Page/ImageModal.vue
+++ portfolio-new/src/components/Page/ImageModal.vue
@@ -1,22 +1,38 @@
-<!-- included in certificates.vue -->
 <template>
+  <!-- Overlay -->
   <div
     class="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
     @click.self="emit('close')"
   >
-    <div class="bg-white p-4 rounded-xl max-w-3xl w-full">
-      <img :src="image" class="rounded-lg w-full h-auto aspect-[1/1] sm:aspect-[4/3]
-" />
-      <button
-      class="w-full max-w-sm  lg:max-w-none
-           rounded-xl bg-transparent content-wrapper"
-        @click="emit('close')"
-      >
-        Close
-      </button>
+    <!-- Modal -->
+    <div class="bg-white p-4 rounded-xl max-w-3xl  mx-4">
+
+      <!-- Image wrapper (relative) -->
+      <div class="relative">
+        <!-- Close button INSIDE image -->
+        <button
+          class="absolute top-2 right-2
+                 bg-black/70 text-white
+                 w-8 h-8 rounded-full
+                 flex items-center justify-center
+                 hover:bg-black transition z-10"
+          @click="emit('close')"
+        >
+          ✕
+        </button>
+
+        <!-- Image -->
+        <img
+          :src="image"
+          class="rounded-lg w-full h-auto
+                 aspect-[1/1] sm:aspect-[4/3]
+                 object-contain"
+        />
+      </div>
     </div>
   </div>
 </template>
+
 <script setup>
 defineProps({
   image: String,
