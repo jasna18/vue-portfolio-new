f51c6357ceb13373cd5c59fb8655788721514973
jasna18 <jasnamnaz@gmail.com>
2025-12-19 22:33:44 +0400

    added mode as project

--- portfolio-new/src/components/Page/Projects.vue
+++ portfolio-new/src/components/Page/Projects.vue
@@ -1,5 +1,26 @@
+
 <template>
-  <div class="text-center text-gray-600">
-    🚀 Projects content goes here
-  </div>
+     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 
+     sm:gap-1 md:gap-4 px-6 lg:gap-4 mb-6 hover:scale-[1.02] transition-transform
+">
+ <CertificateCard 
+        v-for="project in projects"
+        :key="project.id"
+        :title="project.title"
+        :image="project.image"
+        :link="project.link"
+         mode="project"
+
+        class="w-full object-cover rounded-lg overflow-hidden"
+
+      />
+</div>
 </template>
+<script setup>
+  import { ref } from "vue"
+  import CertificateCard from "../composobles/CertificateCard.vue"
+  import { projects } from '../../data/project.js'
+  import ImageModal from "../Page/ImageModal.vue"
+  const selectedImage = ref(null)
+
+</script>
