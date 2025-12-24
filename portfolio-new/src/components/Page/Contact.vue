c76f24b2b3e576645d96806f96d9404f268d7bcb
jasna18 <jasnamnaz@gmail.com>
2025-12-24 09:20:20 +0400

    added contact heading

---
+++ portfolio-new/src/components/Page/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+<section id="about" class="py-2 md:py-2 lg:py-2">
+<!-- all manin wrapper  -->
+<div class=" px-4 lg:px-8 w-full lg:mx-autopy-4 md:py-6 dark:bg-gray-900 text-gray-900 dark:text-gray-100 xl:justify-center">
+      <div class="text-center mb-4">
+        <h2 class="text-xl md:text-3xl font-extrabold text-[#F68537] mb-2" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+          Contact Me
+        </h2>
+        <p class="text-sm lg:text-md md:text-md text-gray-600 dark:text-gray-300" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+          <span class="font-medium">✨</span>Got a question? Send me a message, and I'll get back to you soon.<span class="font-medium"> ✨</span>
+        </p>
+      </div>
+      </div>
+    </section>
+</template>
